18aa85d80eed9df48d63b518d5d2e7bd07614a61
Amjad Jibon <amjad.jibon@gmail.com>
2024-08-15 13:36:34 +0800

    testing main file

---
+++ testing/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+)
+
+func main() {
+	re := lipgloss.NewRenderer(os.Stdout)
+	labelStyle := re.NewStyle().Foreground(lipgloss.Color("241"))
+	columnStyle := re.NewStyle().Padding(0, 0, 0, 2).Foreground(lipgloss.Color("241"))
+
+	board := [][]string{
+		{"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"},
+		{"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"},
+		{"♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"},
+	}
+
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderRow(true).
+		BorderColumn(true).
+		Rows(board...).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			return lipgloss.NewStyle().
+				Padding(0, 2)
+		})
+
+	ranks := columnStyle.Render(strings.Join([]string{" A", "B", "C", "D", "E", "F", "G", "H  "}, "   "))
+	files := labelStyle.Render(strings.Join([]string{" 1", "2", "3", "4", "5", "6", "7", "8 "}, "\n\n "))
+
+	fmt.Println(lipgloss.JoinVertical(
+		lipgloss.Right,
+		lipgloss.JoinHorizontal(
+			lipgloss.Center,
+			files,
+			t.Render(),
+		), ranks) + "\n")
+}
